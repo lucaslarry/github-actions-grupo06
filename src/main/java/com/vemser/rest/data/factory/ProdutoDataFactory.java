b50adb20fe28e118aa466c557cc173837e545486
Marcelo Dalcin <marcelo_sdalcin@hotmai.com>
2025-02-18 16:15:54 -0300

    'waiting-login'

---
+++ src/main/java/com/vemser/rest/data/factory/ProdutoDataFactory.java
@@ -0,0 +1,9 @@
+package com.vemser.rest.data.factory;
+
+import net.datafaker.Faker;
+import java.util.Locale;
+
+public class ProdutoDataFactory {
+    static Faker faker = new Faker(new Locale("PT-BR"));
+
+}
---
+++ src/main/java/com/vemser/rest/model/Produto.java
@@ -0,0 +1,16 @@
+package com.vemser.rest.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Produto
+{
+    private String nome;
+    private int preco;
+    private int quantidade;
+    private String descricao;
+}
